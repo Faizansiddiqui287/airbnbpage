eb12fa000cf6a7f33e65d859cbfcdab919d38722
Faizan Siddiqui <91340388+Faizansiddiqui287@users.noreply.github.com>
2022-11-20 01:20:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import airbnb from './airbnb.png'
+
+function App() {
+  // #FF385C
+  return (
+    <div className="App">
+      <div>
+   <h1>The Evolution of</h1>
+   <h1>Airbnb's Frontend</h1>
+    <p>@spikebrehm</p>
+    <img src={airbnb} style={{height:'90px', marginTop:'10px'}} alt="img"/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
